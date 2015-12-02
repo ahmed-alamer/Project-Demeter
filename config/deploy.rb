c6dd4c9d22faa0b17e9faf9ffc4f96177019cd21
Ahmed <ahmed_isam@aol.com>
2015-12-01 23:46:01 -0500

    fixed secrets

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :deploy_to, '/home/deploy/project_demeter'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
@@ -36,13 +36,13 @@ set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 
 namespace :deploy do
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      within release_path do
-        execute :rake, 'cache:clear'
-      end
-    end
-  end
+  # after :restart, :clear_cache do
+  #   on roles(:web), in: :groups, limit: 3, wait: 10 do
+  #     # Here we can do anything such as:
+  #     within release_path do
+  #       execute :rake, 'cache:clear'
+  #     end
+  #   end
+  # end
 
 end
