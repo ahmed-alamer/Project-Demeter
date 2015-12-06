ef37ad3986e6ccd4df8768a03a3a218a6d4f7773
Ahmed <ahmed_isam@aol.com>
2015-12-05 22:16:43 -0500

    renamed project to facility and claimant to legal entity

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 	def solar_coin_amount(amount)
-		"#{number_with_delimiter(amount)} SLR"
+		number_with_delimiter(amount, separator: '.', delimiter: ',') + ' SLR'
 	end
 
 end
--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,5 +1,12 @@
 module ProjectsHelper
 	def get_button_link(button_title, target_view)
-		link_to button_title, projects_path(:status => target_view), :class => "btn btn-default #{@view_items == target_view ? 'active' : ''}"
-	end
+		projects_path = projects_path(:status => target_view)
+		link_to button_title,
+						projects_path,
+						:class => "btn btn-default #{@view_items == target_view ? 'active' : ''}"
+  end
+
+  def display_nameplate(nameplate)
+    number_to_human(nameplate, units: {unit: 'kW', thousand: 'mW', million: 'gW'})
+  end
 end
